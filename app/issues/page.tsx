ffe0b6b838053aeee7097674d59e5a101a49d2cb
QuangMinh94 <quangminhdinh38@gmail.com>
2023-10-10 14:21:32 +0700

    set up MUI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,7 +2,11 @@ import { Button } from "@mui/material"
 
 const IssuesPage = () => {
     return (
-        <div><Button variant="contained">New issue</Button></div>
+        <div><Button variant="contained"
+            className="bg-blue-500">
+            New issue
+        </Button>
+        </div>
     )
 }
 
