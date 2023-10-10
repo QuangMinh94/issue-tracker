a928975efb0cdc4cf7cfce7fdffc077936de3709
QuangMinh94 <quangminhdinh38@gmail.com>
2023-10-10 15:29:35 +0700

    Handle error

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -3,15 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from 'zod';
 
 const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
+    title: z.string().min(1, 'Title is required').max(255),
+    description: z.string().min(1, 'Desciption is required')
 })
 
 export async function POST(request: NextRequest) {
     const response = await request.json();
     const validation = createIssueSchema.safeParse(response)
     if (!validation.success)
-        return NextResponse.json(validation.error.errors, { status: 400 })
+        return NextResponse.json(validation.error.format(), { status: 400 })
 
     const newIssue = await prisma.issue.create({
         data: {
