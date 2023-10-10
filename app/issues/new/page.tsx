c65c5f7af140bd79b1d40caeaa87dccac3ec741f
QuangMinh94 <quangminhdinh38@gmail.com>
2023-10-10 15:05:21 +0700

    Handle form submition

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,25 +1,46 @@
 'use client'
 
 import { Button, TextField } from "@mui/material";
+import axios from 'axios';
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { Controller, useForm } from 'react-hook-form';
 import SimpleMDE from "react-simplemde-editor";
 
+interface IssueForm {
+    title: string,
+    description: string
+}
+
 const NewIssuePage = () => {
+    const route = useRouter()
+    const { register, control, handleSubmit } = useForm<IssueForm>()
+
     return (
         //add text file component
-        <div className="max-w-xl space-y-3">
+        <form className="max-w-xl space-y-3"
+            onSubmit={handleSubmit(async (data) => {
+                axios.post('/api/issues', data)
+                route.push('/issues')
+            })}>
             <TextField
                 fullWidth
                 required
                 id="outlined-required"
                 label="Required"
                 placeholder="title"
+                {...register('title')}
             />
-            <SimpleMDE placeholder="Description" />
-            <Button variant="contained"
+            <Controller
+                name='description'
+                control={control}
+                render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+            />
+
+            <Button type="submit" variant="contained"
                 className="bg-blue-500">
                 Submit new issue</Button>
-        </div>
+        </form>
     )
 }
 
