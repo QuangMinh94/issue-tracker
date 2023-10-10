0030bbb0ec3008e20b261af6543fc7d71e5a7ac3
QuangMinh94 <quangminhdinh38@gmail.com>
2023-10-10 16:53:01 +0700

    Show the issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,17 +1,33 @@
-import { Button } from "@mui/material"
-import Link from "next/link"
+import CustomTable from "@/components/CustomTable";
+import prisma from "@/prisma/client";
+import { Button } from "@mui/material";
+import Link from "next/link";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+    const issues = await prisma.issue.findMany();
+    const rows: any[] = []
+    issues.forEach((issue) => {
+        rows.push({
+            key: issue.id,
+            issue: issue.title,
+            status: issue.status,
+            createdAt: issue.createdAt.toDateString()
+        })
+    })
     return (
-        <div>
-
-            <Button variant="contained"
-                className="bg-blue-500">
-                <Link href='/issues/new'>
-                    New issue
-                </Link>
-            </Button>
-        </div>
+        <>
+            <div className="mb-5">
+                <Button variant="contained"
+                    className="bg-blue-500">
+                    <Link href='/issues/new'>
+                        New issue
+                    </Link>
+                </Button>
+            </div>
+            <div>
+                <CustomTable data={rows}></CustomTable>
+            </div>
+        </>
     )
 }
 
