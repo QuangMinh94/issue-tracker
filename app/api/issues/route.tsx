c326a54887cecb140777d2574174df008c679b1a
QuangMinh94 <quangminhdinh38@gmail.com>
2023-10-11 17:39:04 +0700

    Build an API for update

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,10 +1,10 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { createIssueSchema } from "../../validationSchema";
+import { issueSchema } from "../../validationSchema";
 
 export async function POST(request: NextRequest) {
     const response = await request.json();
-    const validation = createIssueSchema.safeParse(response)
+    const validation = issueSchema.safeParse(response)
     if (!validation.success)
         return NextResponse.json(validation.error.format(), { status: 400 })
 
