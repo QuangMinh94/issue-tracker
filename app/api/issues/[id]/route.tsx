6c3486dcbb0343b85e446ca38c91d52c66b077d2
QuangMinh94 <quangminhdinh38@gmail.com>
2023-10-12 12:01:23 +0700

    Build an api for delete

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -26,3 +26,18 @@ export async function PATCH(request: NextRequest,
 
     return NextResponse.json(updateIssue)
 }
+
+export async function DELETE(request: NextRequest,
+    { params }: { params: { id: string } }) {
+    const issue = await prisma.issue.findUnique({
+        where: { id: parseInt(params.id) }
+    })
+
+    if (!issue) return NextResponse.json({ error: 'Invalid issue' }, { status: 400 })
+
+    await prisma.issue.delete({
+        where: { id: issue.id },
+    })
+
+    return NextResponse.json({})
+}
