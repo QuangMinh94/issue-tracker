b9b5584a12051b7330592dd4a697e5f2129dfca3
QuangMinh94 <quangminhdinh38@gmail.com>
2023-10-13 11:33:55 +0700

    Secure the application

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,7 @@
+import authOptions from "@/app/auth/authOptions"
 import prisma from "@/prisma/client"
 import { Col, Row, Space } from "antd"
+import { getServerSession } from "next-auth"
 import { notFound } from "next/navigation"
 import DeleteIssueButton from "./DeleteIssueButton"
 import EditIssueButton from "./EditIssueButton"
@@ -10,6 +12,7 @@ interface Props {
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
+    const session = await getServerSession(authOptions)
     const issueDetail = await prisma.issue.findUnique(
         { where: { id: parseInt(params.id) } }
     )
@@ -21,12 +24,13 @@ const IssueDetailPage = async ({ params }: Props) => {
             <Col xs={24} md={18} lg={18}>
                 <IssueDetails issue={issueDetail} />
             </Col>
-            <Col xs={24} md={6} lg={6}>
-                <Space className="w-full" direction='vertical' size={10}>
-                    <EditIssueButton issueId={issueDetail.id} />
-                    <DeleteIssueButton issueId={issueDetail.id} />
-                </Space>
-            </Col>
+            {session &&
+                <Col xs={24} md={6} lg={6}>
+                    <Space className="w-full" direction='vertical' size={10}>
+                        <EditIssueButton issueId={issueDetail.id} />
+                        <DeleteIssueButton issueId={issueDetail.id} />
+                    </Space>
+                </Col>}
         </Row>
     )
 }
