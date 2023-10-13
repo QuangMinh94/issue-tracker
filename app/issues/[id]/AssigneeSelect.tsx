b8e9d8ffcf44f02887e9ae7508481716b9d29b4d
QuangMinh94 <quangminhdinh38@gmail.com>
2023-10-13 11:58:40 +0700

    Populated Assignee component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,8 +1,26 @@
 'use client'
 
+import { User } from '@prisma/client'
 import { Select } from 'antd'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const AssigneeSelect = () => {
+    //const [users, setUsers] = useState<User[]>([])
+    const [option, setOptions] = useState<any[]>([])
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const { data } = await axios.get<User[]>('/api/users')
+            const _option: any[] = []
+            data.forEach((user) => {
+                _option.push({ label: user.name, value: user.id })
+            })
+            setOptions(_option)
+        }
+        fetchUser()
+    }, [])
+
     return (
         <Select
             className='w-full'
@@ -10,12 +28,7 @@ const AssigneeSelect = () => {
             //defaultValue="lucy"
             //style={{ width: 120 }}
             //onChange={handleChange}
-            options={[
-                { value: 'jack', label: 'Jack' },
-                { value: 'lucy', label: 'Lucy' },
-                { value: 'Yiminghe', label: 'yiminghe' },
-                { value: 'disabled', label: 'Disabled', disabled: true },
-            ]}
+            options={option}
         />
     )
 }
