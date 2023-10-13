308a8e7b0172db4d70e9bd6cc7aefb00a4014bca
QuangMinh94 <quangminhdinh38@gmail.com>
2023-10-13 11:08:54 +0700

    Refactor navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,58 +8,73 @@ import { usePathname } from "next/navigation"
 import { AiFillBug } from 'react-icons/ai'
 
 const NavBar = () => {
-    const currentPath = usePathname();
-    const { status, data: session } = useSession();
-    const links = [
-        { label: 'Dashboard', href: '/' },
-        { label: 'Issues', href: '/issues' },
-    ]
+
 
     return (
         <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center justify-between">
 
             <div className="flex items-center gap-3">
-                <Link href='/'><AiFillBug /></Link>
-                <ul className="flex space-x-6">
-                    {links.map((link) => (
-                        <li key={link.href}>
-                            <Link
-                                //className={`${link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500 hover:text-zinc-800 transition-colors'}`}
-                                className={classnames({
-                                    'text-zinc-900': link.href === currentPath,
-                                    'text-zinc-500': link.href !== currentPath,
-                                    'hover:text-zinc-800 transition-colors': true
-                                })}
-                                href={link.href}>
-                                {link.label}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
+                <Link href='/'>
+                    <AiFillBug />
+                </Link>
+                <NavLink />
             </div>
-            <div>
-                {status === 'authenticated' &&
-                    <Dropdown menu={{
-                        items: [{
-                            label: session.user?.email,
-                            key: '0',
-                        }, {
-                            label: (<Link href='api/auth/signout'>Logout</Link>),
-                            key: '1',
-                        }]
-                    }}>
-                        <Avatar
-                            className='cursor-pointer'
-                            src={session?.user!.image}
-                            alt='?' />
-                    </Dropdown>
-                }
-                {/* {status === 'authenticated' && <Link href='api/auth/signout'>Logout</Link>} */}
-                {status === 'unauthenticated' && <Link href='api/auth/signin'>Login</Link>}
-            </div>
-
+            <AuthStatus />
         </nav>
     )
+}
+
+const NavLink = () => {
+    const currentPath = usePathname();
+
+    const links = [
+        { label: 'Dashboard', href: '/' },
+        { label: 'Issues', href: '/issues' },
+    ]
+
+    return <ul className="flex space-x-6">
+        {links.map((link) => (
+            <li key={link.href}>
+                <Link
+                    //className={`${link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500 hover:text-zinc-800 transition-colors'}`}
+                    className={classnames({
+                        'nav-link': true,
+                        '!text-zinc-900': link.href === currentPath,
+                    })}
+                    href={link.href}>
+                    {link.label}
+                </Link>
+            </li>
+        ))}
+    </ul>
+}
+
+const AuthStatus = () => {
+    const { status, data: session } = useSession();
+
+    if (status === 'loading') return null
+
+    if (status === 'unauthenticated')
+        return <Link className='nav-link' href='api/auth/signin'>Login</Link>
+
+    return <div>
+        {status === 'authenticated' &&
+            <Dropdown menu={{
+                items: [{
+                    label: session.user?.email,
+                    key: '0',
+                }, {
+                    label: (<Link href='api/auth/signout'>Logout</Link>),
+                    key: '1',
+                }]
+            }}>
+                <Avatar
+                    className='cursor-pointer'
+                    src={session?.user!.image}
+                    alt='?' />
+            </Dropdown>
+        }
+    </div>
 }
 
 export default NavBar
