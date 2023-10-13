f8d543f2d65443719dc6bcc33a055a05cc756e13
Minh Dinh <quangminhdinh38@gmail.com>
2023-10-14 01:03:52 +0700

    Add server side rendering for antd

--- app/globalTheme.tsx
+++ app/globalTheme.tsx
@@ -1,23 +1,24 @@
 'use client'
 
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useServerInsertedHTML } from 'next/navigation'
+import { PropsWithChildren, useState } from 'react'
 
-type Prop = {
-    children: any
-}
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs'
 
-const GlobalTheme = ({ children }: Prop) => {
-    const theme = createTheme({
-        typography: {
-            fontFamily: 'inherit'
-        }
+const AntdProvider = ({ children }: PropsWithChildren) => {
+    const [cache] = useState(() => createCache())
+
+    useServerInsertedHTML(() => {
+        return (
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `</script>${extractStyle(cache)}<script>`,
+                }}
+            />
+        )
     })
 
-    return (
-        <ThemeProvider theme={theme}>
-            {children}
-        </ThemeProvider>
-    )
+    return <StyleProvider cache={cache}>{children}</StyleProvider>
 }
 
-export default GlobalTheme
+export default AntdProvider
