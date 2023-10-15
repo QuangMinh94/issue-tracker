e81b7fa32bf21125bc386be93322b1c9e01ecdf3
Minh Dinh <quangminhdinh38@gmail.com>
2023-10-16 00:43:01 +0700

    Build a filter component

--- app/issues/issueActions.tsx
+++ app/issues/issueActions.tsx
@@ -1,16 +1,15 @@
-import { Button } from "antd";
-import Link from "next/link";
+import { Button, Flex } from 'antd'
+import Link from 'next/link'
+import IssueStatusFilter from './IssueStatusFilter'
 
 const IssueActions = () => {
     return (
-        <div className="mb-5">
-            <Button type='primary'
-                className="bg-blue-500">
-                <Link href='/issues/new'>
-                    New issue
-                </Link>
+        <Flex className="mb-5" justify="space-between">
+            <IssueStatusFilter />
+            <Button type="primary" className="bg-blue-500">
+                <Link href="/issues/new">New issue</Link>
             </Button>
-        </div>
+        </Flex>
     )
 }
 
