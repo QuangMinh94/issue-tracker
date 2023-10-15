7b641537b76a672097ea9e81448c91536018864a
Minh Dinh <quangminhdinh38@gmail.com>
2023-10-16 04:44:02 +0700

    Issue chart compnent

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import prisma from '@/prisma/client'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import IssueSummary from './IssueSummary'
+import IssueChart from './IssueChart'
 export default async function Home() {
     const open = await prisma.issue.count({
         where: { status: 'OPEN' },
@@ -12,5 +12,5 @@ export default async function Home() {
         where: { status: 'CLOSED' },
     })
 
-    return <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+    return <IssueChart open={open} inProgress={inProgress} closed={closed} />
 }
