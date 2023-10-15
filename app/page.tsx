7088f448a63d9d9af2d7c9c573b3c8468a0899a4
Minh Dinh <quangminhdinh38@gmail.com>
2023-10-16 04:49:47 +0700

    Layout the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import prisma from '@/prisma/client'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import { Col, Flex, Row } from 'antd'
 import IssueChart from './IssueChart'
+import IssueSummary from './IssueSummary'
+import LatestIssues from './LatestIssues'
 export default async function Home() {
     const open = await prisma.issue.count({
         where: { status: 'OPEN' },
@@ -12,5 +15,25 @@ export default async function Home() {
         where: { status: 'CLOSED' },
     })
 
-    return <IssueChart open={open} inProgress={inProgress} closed={closed} />
+    return (
+        <Row gutter={15}>
+            <Col xs={24} md={12} lg={12}>
+                <Flex vertical gap={6}>
+                    <IssueSummary
+                        open={open}
+                        inProgress={inProgress}
+                        closed={closed}
+                    />
+                    <IssueChart
+                        open={open}
+                        inProgress={inProgress}
+                        closed={closed}
+                    />
+                </Flex>
+            </Col>
+            <Col xs={24} md={12} lg={12}>
+                <LatestIssues />
+            </Col>
+        </Row>
+    )
 }
