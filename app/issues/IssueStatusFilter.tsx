d357e848f0c902596ae06ef13df36eb178d35d9e
Minh Dinh <quangminhdinh38@gmail.com>
2023-10-16 02:21:46 +0700

    Fix filtering bug

--- app/issues/IssueStatusFilter.tsx
+++ app/issues/IssueStatusFilter.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Select } from 'antd'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 const statuses = [
     {
@@ -24,15 +24,26 @@ const statuses = [
 
 const IssueStatusFilter = () => {
     const router = useRouter()
+    const searchParam = useSearchParams()
+
+    const onSelectStatus = (status: string) => {
+        const params = new URLSearchParams()
+        if (status) params.append('status', status)
+        if (searchParam.get('orderBy'))
+            params.append('orderBy', searchParam.get('orderBy')!)
+        const query = params.size ? '?' + params.toString() : ''
+        router.push('/issues' + query)
+    }
+
     return (
         <Select
+            defaultValue={
+                searchParam.get('status') ? searchParam.get('status') : ''
+            }
             className="w-48"
             placeholder="Filter by status"
             options={statuses}
-            onSelect={(status) => {
-                const query = status ? `?status=${status}` : ''
-                router.push('/issues' + query)
-            }}
+            onSelect={onSelectStatus}
         ></Select>
     )
 }
