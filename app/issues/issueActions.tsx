23361cfe7b145a1cbcec871dfb2e3d49b53ab8d9
Minh Dinh <quangminhdinh38@gmail.com>
2023-10-16 03:39:26 +0700

    Refactor issueTable

--- app/issues/issueActions.tsx
+++ app/issues/issueActions.tsx
@@ -4,7 +4,7 @@ import IssueStatusFilter from './IssueStatusFilter'
 
 const IssueActions = () => {
     return (
-        <Flex className="mb-5" justify="space-between">
+        <Flex justify="space-between">
             <IssueStatusFilter />
             <Button type="primary" className="bg-blue-500">
                 <Link href="/issues/new">New issue</Link>
